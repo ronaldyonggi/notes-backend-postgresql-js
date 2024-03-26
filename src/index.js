456c4b87f8f71c63987fb902ea28a6057b7221dc
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-26 08:52:22 +0800

    use usersRouter and loginRouter

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const app = express()
 
 const { connectToDatabase } = require('./utils/db')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const { PORT } = require('./utils/config')
 
 app.use(express.json())
+
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
