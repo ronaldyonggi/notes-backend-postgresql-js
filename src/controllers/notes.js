834a46a9baea60ee9f78f3346e85e816421616b6
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-26 11:19:28 +0800

    define token extractor middleware

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router()
 const { Note, User } = require('../models')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../utils/config')
 
 // Middleware for finding a specific note
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id)
+  next()
+}
+
+// Middleware for tokens
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid '})
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing'})
+  }
   next()
 }
 
