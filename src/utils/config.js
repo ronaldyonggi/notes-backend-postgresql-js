ae3f43a049a3b9f823cfdd0caff7fc341e693629
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-26 11:19:08 +0800

    add SECRET env var

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
 const DATABASE_URL = process.env.DATABASE_URL
+const SECRET = process.env.SECRET
 
 module.exports = {
   DATABASE_URL,
-  PORT
+  PORT,
+  SECRET
 }
