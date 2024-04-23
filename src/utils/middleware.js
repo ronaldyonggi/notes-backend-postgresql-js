36ef9e183f2f2f31b7ed309b0e628baecb6343f4
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-23 08:32:27 +0700

    tokenExtractor add additional res

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -6,7 +6,13 @@ const { User } = require('../models');
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization');
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: 'token invalid' });
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' });
   }
 
   next();
